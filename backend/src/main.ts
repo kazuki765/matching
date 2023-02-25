ca207164f16a96163b3fe4c287818e0077a36e8c
kazuki765 <kazuki.kujira@gmail.com>
2023-02-25 09:05:20 +0000

    precommitの差分

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
-const env = process.env;
+const env = process.env
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(env.BACKEND_PORT);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(env.BACKEND_PORT)
 }
-bootstrap();
+bootstrap()
